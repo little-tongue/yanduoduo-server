63dfd172f201f8f936bcf42670678b8acb2c0a4f
biu <hezhou920@gmail.com>
2018-06-27 14:01:24 +0800

    添加路由

---
+++ app/controller/user.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class UserController extends Controller {
+  async getPhoneCode() {
+    // TODO
+  }
+
+  async register() {
+    // TODO
+  }
+
+  async login() {
+    // TODO
+  }
+
+  async resetPassword() {
+    // TODO
+  }
+
+  async refreshToken() {
+    // TODO
+  }
+
+  async getProfile() {
+    // TODO
+  }
+
+  async uploadAvatar() {
+    // TODO
+  }
+
+  async exitLogin() {
+    // TODO
+  }
+}
+
+module.exports = UserController;
--- app/router.js
+++ app/router.js
@@ -1,9 +1,20 @@
 'use strict';
 
+const BASE_URL = '/api/v1';
+
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
+  const { user: userController } = controller;
+
+  router.get(BASE_URL + '/smsCode', userController.getPhoneCode); // 获取短信验证码
+  router.post(BASE_URL + '/register', userController.register); // 注册
+  router.post(BASE_URL + '/login', userController.login); // 登陆
+  router.post(BASE_URL + '/resetPassword', userController.resetPassword); // 重置密码
+  router.get(BASE_URL + '/refresh_token', userController.refreshToken); // 刷新 token
+  router.get(BASE_URL + '/profile', userController.getProfile); // 获取用户信息
+  router.post(BASE_URL + '/avatar', userController.uploadAvatar); // 上传并修改头像
+  router.get(BASE_URL + '/logout', userController.exitLogin); // 退出登录
 };
