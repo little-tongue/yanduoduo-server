86e662efd1f85ad349d911875b2fb0c97a74acdf
biu <hezhou920@gmail.com>
2018-08-03 14:18:53 +0800

    docs: 更新接口文档，调整相应接口

--- app/controller/login.js
+++ app/controller/login.js
@@ -12,6 +12,29 @@ const validateRules = require('../core/validate_rules');
 const CustomError = require('../core/error/CustomError');
 
 class LoginController extends Controller {
+  async getPhoneCode() {
+    const { ctx } = this;
+    const { logger } = ctx;
+
+    try {
+      ctx.validate({
+        phone: validateRules.phone,
+      }, ctx.query);
+    } catch (err) {
+      logger.info(err.errors);
+      return this.fail(new CustomError(CustomError.TYPES.invalidParam));
+    }
+
+    try {
+      await ctx.service.user.sendPhoneCode(ctx.query.phone);
+      this.success();
+      logger.info('成功发送验证码');
+    } catch (err) {
+      logger.warn(err);
+      this.fail(err);
+    }
+  }
+
   async login() {
     const { ctx } = this;
     const { logger } = ctx;
--- app/router.js
+++ app/router.js
@@ -7,14 +7,15 @@ const BASE_URL = '/v1';
  */
 module.exports = app => {
   const { router, controller } = app;
-  const { user: userController } = controller;
+  const { user: userController, login: loginController } = controller;
   const auth = app.middleware.auth;
 
-  router.get(BASE_URL + '/smsCode', userController.getPhoneCode); // 获取短信验证码
-  router.post(BASE_URL + '/login', userController.login); // 登陆
-  router.post(BASE_URL + '/resetPassword', userController.resetPassword); // 重置密码
-  router.get(BASE_URL + '/refreshToken', userController.refreshToken); // 刷新 token
-  router.get(BASE_URL + '/profile', auth, userController.getProfile); // 获取用户信息
-  router.post(BASE_URL + '/avatar', auth, userController.uploadAvatar); // 上传并修改头像
-  router.get(BASE_URL + '/logout', auth, userController.exitLogin); // 退出登录
+  router.get(BASE_URL + '/smsCode', loginController.getPhoneCode); // 获取短信验证码
+  router.post(BASE_URL + '/login', loginController.login); // 登陆
+  router.get(BASE_URL + '/refreshToken', loginController.refreshToken); // 刷新 token
+  router.get(BASE_URL + '/logout', auth, loginController.exitLogin); // 退出登录
+
+  router.get(BASE_URL + '/userinfo', auth, userController.getUserInfo); // 获取用户信息
+  router.post(BASE_URL + '/user/avatar', auth, userController.uploadAvatar); // 上传并修改头像
+  router.post(BASE_URL + '/user/resetPassword', userController.resetPassword); // 重置密码
 };
